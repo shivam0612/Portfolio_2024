a97bb38ee24a47262d393a1af35c823b69c558cb
shivam0612 <shivampatel868@yahoo.com>
2024-06-01 15:07:18 -0400

    Modify email and footer section

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,14 @@ import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
+import { ToastContainer } from "react-toastify";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+
       <Navbar />
+      <ToastContainer/>
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AchievementsSection />
